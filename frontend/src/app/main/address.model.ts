3ee3fef79cbc0f8495e0ea829a1f3b411b1b3dde
Nicolas Giachino <nicolas.giachino@etu.u-bordeaux.fr>
2019-11-25 13:12:31 +0100

    ajout interface retournée par l'API Addresse

---
+++ frontend/src/app/main/address.model.ts
@@ -0,0 +1,37 @@
+export interface AddressProperties {
+  label: string;
+  score: number;
+  housenumber: string;
+  id: string;
+  type: string;
+  name: string;
+  postcode: string;
+  citycode: string;
+  x: number;
+  y: number;
+  city: string;
+  context: string;
+  importance: number;
+  street: string;
+}
+
+export interface AddressGeometry {
+  type: string;
+  coordinates: number[];
+}
+
+export interface Address {
+  type: string;
+  geometry: AddressGeometry;
+  properties: AddressProperties;
+}
+
+export interface AddressList {
+  type: string;
+  version: string;
+  features: Address[];
+  attribution: string;
+  license: string;
+  query: string;
+  limit: number;
+}
