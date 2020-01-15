f576d84a55f2384a33d2fbfb633690235f14161e
aouldamara <8c561be7>
2020-01-15 15:20:26 +0100

    Ajout du schéma de donnée de Véhicule

---
+++ backend/services/vehicles.js
@@ -0,0 +1,25 @@
+const database = require('../database/connection');
+
+const db = database.connect();
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://localhost:27017/wheretopark`,
+  {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Vehicles =  mongoose.Schema({
+  _id:{
+    type: Number,
+    required :"ID du véhicule"
+  },
+  userId:{
+    type : Number,
+    required :"ID du propriétaire du véhicule"
+  },
+  isElectric:{
+    type : String,
+    required: "Type de véhicule de la personne?"
+  },
+  height:{
+    type: Number,
+    required :"Le Gabari du véhicule"
+  }
+});
