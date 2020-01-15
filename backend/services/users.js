5f9d4367251e8a8fb3abef1dc9a39564c2d9970d
aouldamara <8c561be7>
2020-01-15 16:18:57 +0100

    Suppression d'un véhicule et d'un user

--- backend/services/users.js
+++ backend/services/users.js
@@ -3,44 +3,53 @@ const database = require('../database/connection');
 const db = database.connect();
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb://localhost:27017/wheretopark`,
-  {useNewUrlParser: true, useUnifiedTopology: true});
+    {useNewUrlParser: true, useUnifiedTopology: true});
 
-const UserModel = new mongoose.Schema({
-  _id : {
-    type : Number,
-    required: "id du user"
+const Users = new mongoose.Schema({
+  _id: {
+    type: Number,
+    required: 'id du user'
   },
-  pseudo:{
-    type : String,
-    required: "pseudo de la personne?"
+  pseudo: {
+    type: String,
+    required: 'pseudo de la personne?'
   },
-  password:{
+  password: {
     type: double,
-    required :"mot de passe de la personne"
+    required: 'mot de passe de la personne'
   }
 });
+const usersModel = mongoose.model('Users', Users);
 
-
-function getUserById(userId){
-  return new Promise((resolve,reject)=>{
-    userModel.find( {_id : userId},(error,user)=>{
+function getUserById(userId) {
+  return new Promise((resolve, reject)=>{
+    usersModel.find( {_id: userId}, (error, user)=>{
       resolve(user);
     });
   });
 }
 
-function getUserByPseudo(pseudo){
-  return new Promise((resolve,reject)=>{
-    userModel.find( {pseudo},(error,user)=>{
+function getUserByPseudo(pseudo) {
+  return new Promise((resolve, reject)=>{
+    usersModel.find( {pseudo}, (error, user)=>{
       resolve(user);
     });
   });
 }
 
-function createUser(pseudo, password){
-  const user =  new userModel({pseudo, password});
-  return new Promise((resolve , reject) =>{
-    user.save((err) => {
+function createUser(pseudo, password) {
+  const user = new usersModel({pseudo, password});
+  return new Promise((resolve, reject) =>{
+    user.save(err => {
+      resolve();
+    });
+  });
+}
+
+function deleteUser(userId) {
+  const user = new usersModel({_id: userId});
+  return new Promise((resolve, reject) =>{
+    user.remove(err => {
       resolve();
     });
   });
@@ -50,5 +59,6 @@ function createUser(pseudo, password){
 module.exports = {
   getUserById,
   getUserByPseudo,
-  createUser
+  createUser,
+  deleteUser
 };
--- backend/services/vehicles.js
+++ backend/services/vehicles.js
@@ -1,26 +1,26 @@
-const database = require('../database/connection');
-
-const db = database.connect();
+// const database = require('../database/connection');
+//
+// const db = database.connect();
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb://localhost:27017/wheretopark`,
-  {useNewUrlParser: true, useUnifiedTopology: true});
+    {useNewUrlParser: true, useUnifiedTopology: true});
 
-const Vehicles =  mongoose.Schema({
-  _id:{
+const Vehicles = mongoose.Schema({
+  _id: {
     type: Number,
-    required :"ID du véhicule"
+    required: 'ID du véhicule'
   },
-  userId:{
-    type : Number,
-    required :"ID du propriétaire du véhicule"
-  },
-  isElectric:{
-    type : String,
-    required: "Type de véhicule de la personne?"
-  },
-  height:{
+  userId: {
     type: Number,
-    required :"Le Gabari du véhicule"
+    required: 'ID du propriétaire du véhicule'
+  },
+  isElectric: {
+    type: String,
+    required: 'Type de véhicule de la personne?'
+  },
+  height: {
+    type: Number,
+    required: 'Le Gabari du véhicule'
   }
 });
 
@@ -37,24 +37,35 @@ function getVehiclesByOwner(userId) {
 
 function getVehiclesById(vehicleId) {
   return new Promise((resolve, reject) =>{
-    VehiclesModel.find( {_id : vehicleId}, (error, vehicles)=>{
+    VehiclesModel.find( {_id: vehicleId}, (error, vehicles)=>{
       resolve(vehicles);
     });
   });
 }
 
 
-function createVehicle(userId, isElectric, height){
-  const vehicle =  new VehiclesModel({userId, isElectric, height });
-  return new Promise((resolve , reject) =>{
-    vehicle.save((err) => {
+function createVehicle(userId, isElectric, height) {
+  const vehicle = new VehiclesModel({userId, isElectric, height});
+  return new Promise((resolve, reject) =>{
+    vehicle.save(err => {
       resolve();
     });
   });
 }
 
+function deleteVehicle(vehicleId) {
+  const vehicle = new VehiclesModel({_id: vehicleId});
+  return new Promise((resolve, reject) =>{
+    vehicle.save(err => {
+      resolve();
+    });
+  });
+}
+
+
 module.exports = {
   getVehiclesByOwner,
   getVehiclesById,
-  createVehicle
+  createVehicle,
+  deleteVehicle
 };
