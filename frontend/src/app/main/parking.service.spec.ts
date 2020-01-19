26b4d9b64b85bee82d0ff3d61f9ba69589dd4a2a
Nicolas Giachino <nicolas.giachino@etu.u-bordeaux.fr>
2020-01-19 20:16:13 +0100

    Mise en place du calcul de priorité de parking

--- frontend/src/app/main/parking.service.spec.ts
+++ frontend/src/app/main/parking.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ParkingService } from './parking.service';
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ParkingService', () => {
   beforeEach(() => TestBed.configureTestingModule({
--- frontend/src/app/main/parking.service.ts
+++ frontend/src/app/main/parking.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { environment } from "../../environments/environment";
-import { Observable } from "rxjs";
-import { Parking } from "./parking.model";
-import { retry } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Parking } from './parking.model';
+import { map, retry } from 'rxjs/operators';
 
 const url = `${environment.backendUrl}/parkings`;
 const numberOfTries = 2;
+const numberOfParkings = 7;
 
 @Injectable({
   providedIn: 'root'
@@ -22,4 +23,42 @@ export class ParkingService {
       retry(numberOfTries)
     );
   }
+
+  public getBestParkings(destination: [number, number]): Observable<Parking[]> {
+    return this.getParkings().pipe(
+      map(parkings => this.initScore(parkings)),
+      map(parkings => this.distanceScore(parkings, destination)),
+      map(parkings => this.sortByScore(parkings)),
+      map(parkings => parkings.map(parking => parking.parking))
+    );
+  }
+
+  private initScore(parkings: Parking[]): ParkingWithScore[] {
+    return parkings.map(parking => ({parking, score: 0}));
+  }
+
+  private distanceScore(parkings: ParkingWithScore[], destination: [number, number]): ParkingWithScore[] {
+    return parkings.map(parking => {
+      const coordinates = parking.parking.location;
+      const distance = Math.sqrt(
+        Math.pow(coordinates[0] - destination[0],2)
+        +
+        Math.pow(coordinates[1] - destination[1],2)
+      );
+      const score = 1000 / distance;
+      return {parking: parking.parking, score: parking.score + score};
+    });
+  }
+
+  private sortByScore(parkings: ParkingWithScore[]): ParkingWithScore[] {
+    parkings.sort(
+      (parkingA, parkingB) => parkingA.score - parkingB.score
+    );
+    return parkings.slice(parkings.length - numberOfParkings - 1, parkings.length);
+  }
+}
+
+interface ParkingWithScore {
+  parking: Parking;
+  score: number;
 }
