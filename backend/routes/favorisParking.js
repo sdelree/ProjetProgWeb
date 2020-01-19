a5a29b8227dac416f7a3a45f407ef0d99aa0ff73
Amel OULD AMARA <ouldamara.amel@gmail.com>
2020-01-19 13:43:08 +0100

    Gestion du nombre de favoris

--- backend/routes/favorisParking.js
+++ backend/routes/favorisParking.js
@@ -6,20 +6,22 @@ const favorisServices = require('../services/favorisParking');
 // GETS
 
 
-
-//POST
+// POST
 router.post('/create', (req, res) =>{
   const name = req.body.name;
   const userId=1;
   const latitude = req.body.latitude;
   const longitude = req.body.longitude;
-
-  favorisServices.createFavoris(name, userId, latitude, longitude).then(favoris=>res.send(favoris));
+  favorisServices.getFavoritesNumber(userId)
+                 .then(size=>{
+                          if (size < 10) {
+                            favorisServices.createFavoris(name, userId, latitude, longitude)
+                                           .then(favorite=>res.send(favorite));
+                          }
+                        })
+                 .catch(err => res.status(401).send(err));
 });
+// PUT
 
 
-//PUT
-
-
-
-//DELETE
+// DELETE
