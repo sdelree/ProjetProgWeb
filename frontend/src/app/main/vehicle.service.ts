f0c1f3fac84269105c37f51fc767760fa76605a0
Sylvain Delrée <sylvain.delree@zen-dws.com>
2020-01-19 16:54:26 +0100

    Implémentation complète du service de gestion des véhicules

--- frontend/src/app/main/vehicle.service.ts
+++ frontend/src/app/main/vehicle.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { Vehicle } from "./vehicle.model";
-import { environment } from "../../environments/environment";
-import { retry } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Vehicle } from './vehicle.model';
+import { environment } from '../../environments/environment';
+import { retry } from 'rxjs/operators';
 
 const url = `${environment.backendUrl}/vehicles`;
 const numberOfTries = 2;
@@ -19,6 +19,26 @@ export class VehicleService {
 
   public getVehicles(): Observable<Vehicle[]> {
     return this.http.get<Vehicle[]>(url).pipe(
+      retry(numberOfTries)
+    );
+  }
+
+  public addVehicle(vehicle: Vehicle): Observable<Vehicle> {
+    return this.http.post<Vehicle>(url, vehicle).pipe(
+      retry(numberOfTries)
+    );
+  }
+
+  public updateVehicle(vehicle: Vehicle): Observable<Vehicle> {
+    const vehicleUrl = `${url}/${vehicle.id}`;
+    return this.http.put<Vehicle>(vehicleUrl, vehicle).pipe(
+      retry(numberOfTries)
+    );
+  }
+
+  public deleteVehicle(vehicle: Vehicle): Observable<void> {
+    const vehicleUrl = `${url}/${vehicle.id}`;
+    return this.http.delete<void>(vehicleUrl).pipe(
       retry(numberOfTries)
     );
   }
