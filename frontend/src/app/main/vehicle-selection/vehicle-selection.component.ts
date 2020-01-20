fd7145e21b3431a6701d93f57c48f633689ac236
Sylvain Delrée <sylvain.delree@zen-dws.com>
2020-01-20 11:47:45 +0100

    Ajout de la logique du composant VehicleSelection

--- frontend/src/app/main/vehicle-selection/vehicle-selection.component.ts
+++ frontend/src/app/main/vehicle-selection/vehicle-selection.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Vehicle } from "../vehicle.model";
 
 @Component({
   selector: 'app-vehicle-selection',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-selection.component.css']
 })
 export class VehicleSelectionComponent implements OnInit {
+  @Input() vehicles: Vehicle[];
+
+  @Output() vehicleSelect: EventEmitter<Vehicle> = new EventEmitter();
+
+  selected: Vehicle;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect() {
+    console.log(this.selected);
+    this.vehicleSelect.emit(this.selected);
   }
 
 }
